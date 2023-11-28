36637e76d91c78713314d488706b1c54e122406d
hugo luiz lazzari dos santos <37092379+katyushi@users.noreply.github.com>
2023-11-28 13:14:49 -0300

    adicionando pacotes e voltando pra vue2

--- note_master/src/store/index.js
+++ note_master/src/store/index.js
@@ -1,6 +1,9 @@
-import { createStore } from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-export default createStore({
+Vue.use(Vuex)
+
+export default new Vuex.Store({
   state: {
   },
   getters: {
@@ -10,5 +13,5 @@ export default createStore({
   actions: {
   },
   modules: {
-  },
-});
+  }
+})
